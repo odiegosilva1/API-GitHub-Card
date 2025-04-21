09c27daa1de9c6c0558cb7ea78a23e0ea5111df2
odiegosilva1 <odiegosilvadev@gmail.com>
2025-04-20 21:19:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,17 +2,41 @@ function fetchGitHubUser(username) {
   fetch(`https://api.github.com/users/${username}`)
     .then((response) => {
       if (!response.ok) {
-        console.error(error);
+        throw new Error("Usuário não encontrado");
       }
       return response.json();
     })
     .then((user) => {
-      console.log("user, user");
+      console.log("user", user); // Objeto do usuário será exibido aqui
       createUserCard(user);
     })
     .catch((error) => {
       console.error(error);
       const app = document.getElementById("app");
-      app.innerHTML = `< p style='color:red>Erro: ${error.message}</p>`;
+      app.innerHTML = `<p style='color:red'>Erro: ${error.message}</p>`;
     });
 }
+
+// Função que Cria o Card (corrigida apenas a linha do avatar_url)
+function createUserCard(user) {
+  const app = document.getElementById("app");
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const avatar = document.createElement("img");
+  avatar.src = user.avatar_url;
+  avatar.alt = `${user.login}s avatar`;
+
+  const name = document.createElement("h2");
+  name.textContent = user.name;
+
+  const login = document.createElement("p");
+  login.textContent = `@${user.login}`;
+
+  card.appendChild(avatar);
+  card.appendChild(name);
+  card.appendChild(login);
+
+  app.appendChild(card);
+}
+fetchGitHubUser("odiegosilva1");
